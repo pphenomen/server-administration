c1c73ab874c7d08cce97ea6e4e36c014e5805096
pphenomen <149307465+pphenomen@users.noreply.github.com>
2025-06-19 16:09:00 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,16 +1,24 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from sqlalchemy import URL
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://kubsu:kubsu@db:5432/kubsu")
+url_object = URL.create(drivername="postgresql+psycopg",
+                 username="kubsu",
+                 password="kubsu",
+                 host="localhost",
+                 port=5432, 
+                 database="kubsu")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+
+engine = create_async_engine(url_object, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
+
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
 
 async def get_db() -> AsyncSession:
     async with AsyncSessionLocal() as session:
